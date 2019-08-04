ef2439c13dd2879a229734b7d70dcaa85d37cf33
DarkCenobyte <darkcenobyte@gmail.com>
2019-08-04 10:55:57 +0200

    Do research about CONTROL/CHECK process and user contacts

--- prayReceiver.js
+++ prayReceiver.js
@@ -2,7 +2,9 @@ const prayResponder = require('./prayResponder.js');
 const { isSameArray } = require('./utils.js');
 
 const PROTOCOL_NET_LINE_REQ = 'NET: LINE_REQ';
-const PROTOCOL_CONTROL = 'NET: CONTROL'; //0x10
+const PROTOCOL_USER_CHECK = 'USER: CHECK'; //0x10
+const PROTOCOL_USER_CHECK_A = 'USER: CHECK_A'; //0x0d
+const PROTOCOL_USER_CHECK_B = 'USER: CHECK_B'; //0x0e
 const PROTOCOL_NET_ULIN = 'NET: ULIN';
 const PROTOCOL_NET_STAT = 'NET: STAT';
 const PROTOCOL_NET_RUSO = 'NET: RUSO';
@@ -25,8 +27,14 @@ function identifyNetBabelType(data) {
     console.log(PROTOCOL_NET_LINE_REQ);
     return PROTOCOL_NET_LINE_REQ;
   } else if (isSameArray(head.data, [0x10, 0x00, 0x00, 0x00])) {
-    console.log(PROTOCOL_CONTROL);
-    return PROTOCOL_CONTROL;
+    console.log(PROTOCOL_USER_CHECK);
+    return PROTOCOL_USER_CHECK;
+  } else if (isSameArray(head.data, [0x0D, 0x00, 0x00, 0x00])) {
+    console.log(PROTOCOL_USER_CHECK_A);
+    return PROTOCOL_USER_CHECK_A;
+  } else if (isSameArray(head.data, [0x0E, 0x00, 0x00, 0x00])) {
+    console.log(PROTOCOL_USER_CHECK_B);
+    return PROTOCOL_USER_CHECK_B;
   } else if (isSameArray(head.data, [0x13, 0x00, 0x00, 0x00])) {
     console.log(PROTOCOL_NET_ULIN);
     return PROTOCOL_NET_ULIN;
@@ -82,8 +90,8 @@ function parsePayload(payload) {
       // TODO
       return null;
       break;
-    case PROTOCOL_CONTROL:
-      // TODO
+    case PROTOCOL_USER_CHECK:
+      // probably answered with 0x0d/0x0e only if something happen server-side
       return null;
       break;
     default:
@@ -111,7 +119,6 @@ async function transmit(payload, socket) {
 
   if ((res.payload || '').length > 0) {
     prayResponder.respond(JSON.parse(res.payload), socket);
-    socket.end();
   }
 }
 
@@ -149,6 +156,7 @@ function init() {
           if (payload !== null) {
             await transmit(payload, socket);
           }
+          socket.end();
         }
       );
       socket.once(
